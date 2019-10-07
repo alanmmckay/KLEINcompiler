aa4c817c7d38d1a8592d230109a53b3ed761186a
alan <alanmmckay@gmail.com>
2019-10-07 12:09:58 -0500

    Expanded LexicalError class. Let me know if any other info should be included in the output file. Also pitch ideas for the ParseError class

--- src/errors.py
+++ src/errors.py
@@ -2,7 +2,20 @@ import sys
 #sys.tracebacklimit=0
 
 # errors thrown by the scanner
-class LexicalError(ValueError):
+class LexicalError():
+    def __init__(self, msg, position, program):
+        self.output_string = "!--- SCANNER ERROR! ---!\n\n\n"
+        self.output_string += "---> Input Program: \n"
+        self.output_string += program + "\n\n\n"
+        self.output_string += "---> Input Program remaining to be scanned: \n"
+        self.pos = position
+        while self.pos < len(program):
+            self.output_string += program[self.pos]
+            self.pos += 1
+        self.output = open("scanner_error.txt", "w")
+        self.output.write(self.output_string)
+        self.output.close()
+        raise ValueError(msg)
     pass
 
 
