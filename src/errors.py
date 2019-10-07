aa093a13bae3c56138370ae4042239ab8585b214
alan <alanmmckay@gmail.com>
2019-10-07 16:14:12 -0500

    Created a global class called GeneralError which LexicalError is a subclass. Need to work on GeneralError being able to raise an exception when it itself is instantiated.

--- src/errors.py
+++ src/errors.py
@@ -1,23 +1,53 @@
 import sys
 #sys.tracebacklimit=0
 
-# errors thrown by the scanner
-class LexicalError():
-    def __init__(self, msg, position, program):
-        self.output_string = "!--- SCANNER ERROR! ---!\n\n\n"
+class GeneralError():
+    def __init__(self, program):
+        self.error_type = ""
+        self.file_name = "error.txt"
+        self.program_string = program
+        self.error_string = "General Error"
+        self.output_string = ""
+        self.output = ""
+        self.error_message = "General Error Thrown!"
+    
+    def open_file(self):
+        self.output = open(self.file_name, "w")
+        
+    def write_file(self, error):
+        self.output_string = "!--- "+self.error_type+" ERROR! ---!\n\n\n"
         self.output_string += "---> Input Program: \n"
-        self.output_string += program + "\n\n\n"
-        self.output_string += "---> Input Program remaining to be scanned: \n"
+        self.output_string += self.program_string
+        self.output_string += "\n\n\n---> Error Information: \n"
+        self.output_string += error
+        self.output.write(self.output_string)
+        
+    def close_file(self):
+        self.output.close()
+        
+    def throw_error(self, msg):
+        raise ValueError(msg)
+    
+    def output_error(self):
+        self.open_file()
+        self.write_file(self.error_string)
+        self.close_file()
+        self.throw_error(self.error_message)
+        pass
+
+class LexicalError(GeneralError):
+    def __init__(self, msg, program, position):
+        GeneralError.__init__(self, program)
+        self.error_type = "SCANNER"
+        self.file_name = "scanner_error.txt"
+        self.error_string = "-> Input Program remaining to be scanned: \n"
+        self.error_message = msg
         self.pos = position
         while self.pos < len(program):
-            self.output_string += program[self.pos]
+            self.error_string += program[self.pos]
             self.pos += 1
-        self.output = open("scanner_error.txt", "w")
-        self.output.write(self.output_string)
-        self.output.close()
-        raise ValueError(msg)
-    pass
-
+        self.output_error()
+        
 
 # errors thrown by the parser
 class ParseError(ValueError):
